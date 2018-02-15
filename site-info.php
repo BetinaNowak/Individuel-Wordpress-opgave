1783c09d45986ffee8d39b7437a0d74472f2967f
BetinaNowak <32088964+BetinaNowak@users.noreply.github.com>
2018-02-15 11:38:00 +0100

    Add files via upload

---
+++ site-info.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen_Child
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<?php printf( __( 'Powered by %s', 'twentyseventeen' ), 'Betina Svendsen' ); ?></a>
+</div><!-- .site-info -->
